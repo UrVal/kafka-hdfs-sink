257770d46767951e5bfe5f92c7bacf8be1189cc6
Andrei Muraru <andreimuraru@Andreis-Mac-mini.local>
2016-09-21 00:43:18 +0300

    Added support for avro and parquet. Added auto-offset.reset param.

--- kafka-hdfs-sink.sbt
+++ kafka-hdfs-sink.sbt
@@ -10,5 +10,9 @@ scalaVersion := "2.10.5"
 resolvers += "Spark Packages Repo" at "http://dl.bintray.com/spark-packages/maven"
 
 
-libraryDependencies += "org.apache.spark" % "spark-streaming_2.10" % "1.2.0"
-libraryDependencies += "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.6.1"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.10" % "1.6.0"
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.6.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
+libraryDependencies += "com.databricks" %% "spark-csv" % "1.5.0"
+libraryDependencies += "com.databricks" %% "spark-avro" % "2.0.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.6.0"
--- src/main/scala/KafkaHDFSSink.scala
+++ src/main/scala/KafkaHDFSSink.scala
@@ -2,49 +2,88 @@ import kafka.serializer.StringDecoder
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.SparkConf
+import com.databricks.spark.csv._
+import com.databricks.spark.avro._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql.SQLContext
 
 object KafkaHDFSSink{
 
   def main(args: Array[String]): Unit = {
-   if (args.length != 3) {
+   if (args.length != 5) {
       System.err.println(s"""
         |Usage: KafkaHDFSSink <brokers> <topics> <destination-url>
         |  <brokers> is a list of one or more Kafka brokers
         |  <topics> is a list of one or more kafka topics to consume from
         |  <destination-url> is the url prefix (eg:in hdfs) into which to save the fragments. Fragment names will be suffixed with the timestamp. The fragments are directories.(eg: hdfs:///temp/kafka_files/)  
-        """.stripMargin)
+        |  <offset_to_start_from> is the position from where the comsumer should start to receive data. Choose between: smallest and largest
+		|  <output_format> is the file format to output the files to. Choose between: parquet, avro and text.
+		""".stripMargin)
       System.exit(1)
     }
    
         //Create SparkContext
     val conf = new SparkConf()
       .setMaster("yarn-client")
-      .setAppName("LowLevelKafkaConsumer")
+      .setAppName("KafkaConsumer")
       .set("spark.executor.memory", "5g")
       .set("spark.rdd.compress","true")
       .set("spark.storage.memoryFraction", "1")
       .set("spark.streaming.unpersist", "true")
 
-     val Array(brokers, topics, destinationUrl) = args
+     val Array(brokers, topics, destinationUrl, offset, outputformat) = args
 
 
-    val sparkConf = new SparkConf().setAppName("KafkaConsumer")
+    val sparkConf = new SparkConf().setAppName("KafkaConsumer_"+topics)
     val ssc = new StreamingContext(sparkConf, Seconds(2))
-
-    val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
+	//SparkSQL
+	val sc = new SparkContext(sparkConf)
+	val sqlContext = new SQLContext(sc)
+	
+	val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers, "auto.offset.reset" -> offset)
 
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topicsSet)
 
-	
-    messages.foreachRDD( rdd =>{
-		if(!rdd.partitions.isEmpty)
-		{
-		 	val timestamp: Long = System.currentTimeMillis / 1000
-		 	rdd.map(_._2).saveAsTextFile(destinationUrl+timestamp)
+	  if(outputformat == "parquet") {
+    	  messages.foreachRDD( rdd =>{
+			  if(!rdd.partitions.isEmpty)
+			  {	
+		 		  val timestamp: Long = System.currentTimeMillis / 1000
+				  val json_rdd =  sqlContext.jsonRDD(rdd.map(_._2))
+				  val df = json_rdd.toDF()
+				  df.write.parquet(destinationUrl+timestamp)
+		 		  //rdd.map(_._2).saveAsTextFile(destinationUrl+timestamp)
+			
 		}
     })
+}
+	  if(outputformat == "avro") {
+    	  messages.foreachRDD( rdd =>{
+			  if(!rdd.partitions.isEmpty)
+			  {	
+		 		  val timestamp: Long = System.currentTimeMillis / 1000
+				  val json_rdd =  sqlContext.jsonRDD(rdd.map(_._2))
+				  val df = json_rdd.toDF()
+				  df.write.avro(destinationUrl+timestamp)
+		 		  //rdd.map(_._2).saveAsTextFile(destinationUrl+timestamp)
+			
+		}
+    })
+}
+	  if(outputformat == "text") {
+    	  messages.foreachRDD( rdd =>{
+			  if(!rdd.partitions.isEmpty)
+			  {	
+		 		  val timestamp: Long = System.currentTimeMillis / 1000
+				  rdd.map(_._2).saveAsTextFile(destinationUrl+timestamp)
+			
+		}
+    })
+}
+	
     
     ssc.checkpoint(destinationUrl+"__checkpoint")
 
