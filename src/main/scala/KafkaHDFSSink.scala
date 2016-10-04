c4f53c118fea330e544e9e00c9298f3f9d1c78fc
Andrei Muraru <andrei.cristian.muraru@gmail.com>
2016-10-04 16:05:26 +0300

    updated for parquet

--- src/main/scala/KafkaHDFSSink.scala
+++ src/main/scala/KafkaHDFSSink.scala
@@ -15,9 +15,10 @@ object KafkaHDFSSink{
    if (args.length != 5) {
       System.err.println(s"""
         |Usage: KafkaHDFSSink <brokers> <topics> <destination-url> <offset_to_start_from> <outputformat>
-        |  <brokers> is a list of one or more Kafka brokers
-        |  <topics> is the topic to read from
-        |  <destination-url> is the url prefix (eg:in hdfs) into which to save the fragments. Fragment names will be suffixed with the timestamp. The fragments are directories.(eg: hdfs:///temp/kafka_files/)  
+        |This consumer assumes data is send in JSON format, if data is not send in readable JSON, it will write the data as text files in hdfs:///malformed_kafka_data/<timestamp>
+        |  <brokers> is a list of one or more Kafka brokers 
+        |  <topic> this is a generic topic to read from
+        |  <destination-url> is the url prefix (eg:in hdfs) into which to save the fragments. Path will be suffixed with @table_name and @schema_version (eg: hdfs:///temp/kafka_files/)  
         |  <offset_to_start_from> is the position from where the comsumer should start to receive data. Choose between: smallest and largest
 		|  <output_format> is the file format to output the files to. Choose between: parquet, avro and json.
 		""".stripMargin)
@@ -32,22 +33,20 @@ object KafkaHDFSSink{
       .set("spark.rdd.compress","true")
       .set("spark.storage.memoryFraction", "1")
       .set("spark.streaming.unpersist", "true")
-	  //.set("spark.driver.allowMultipleContexts", "true")
+	  
+     val Array(brokers, topic, destinationUrl, offset, outputformat) = args
 
-     val Array(brokers, topics, destinationUrl, offset, outputformat) = args
 
-
-    val sparkConf = new SparkConf().setAppName("KafkaConsumer_"+topics)
+    val sparkConf = new SparkConf().setAppName("KafkaConsumer_Generic")
     val sc = new SparkContext(sparkConf)
 	val ssc = new StreamingContext(sc, Seconds(2))
 	//SparkSQL
 	val sqlContext = new SQLContext(sc)
 	
-	val topicsSet = topics.split(",").toSet
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers, "auto.offset.reset" -> offset)
 
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topicsSet)
+      ssc, kafkaParams, topic)
 
     	  messages.foreachRDD( rdd =>{
 			  if(!rdd.partitions.isEmpty)
@@ -55,24 +54,54 @@ object KafkaHDFSSink{
 		 		  val timestamp: Long = System.currentTimeMillis / 1000
 				  if(outputformat == "parquet") {
 					  try {
-				  		  val json_rdd =  sqlContext.jsonRDD(rdd.map(_._2))
+					  		val test_json = json.loads(rdd.map(_._2))
+					  		val json_rdd =  sqlContext.jsonRDD(rdd.map(_._2))
+					  } catch {
+					  		rdd.map(_._2).saveAsTextFile(destinationUrl+"/malformed_kafka_data-"+timestamp+".txt")
+					  }
+					  try {
 				  		  val df = json_rdd.toDF()
-				  		  df.write.mode("append").parquet(destinationUrl+topics)
+				  		  //define table as String
+				  		  val table_name_array = df.select($"@table").limit(1).collect()
+				  		  val table_name_string = table_name_array(0).toString().stripPrefix("[").stripSuffix("]").trim
+				  		  //define schema_version as String
+				  		  val schema_version_array = df.select($"@schema_version").limit(1).collect()
+				  		  val schema_version_string = schema_version_array(0).toString().stripPrefix("[").stripSuffix("]").trim
+				  		  df.write.mode("append").parquet(destinationUrl+table_name_string+"/schema-version-"+schema_version_string)
+				  		  //define p_key as String
+				  		  val primarykey_array = df.select($"@p_key").limit(1).collect()
+				  		  val primarykey_string = primarykey_array(0).toString().stripPrefix("[").stripSuffix("]").trim
+				  		  //define updated as String
+				  		  val updated_array = df.select($"@update").limit(1).collect()
+				  		  val updated_string = updated_array(0).toString().stripPrefix("[").stripSuffix("]").trim
+				  		  //define timestamp as String
+				  		  val updated_array = df.select($"@update").limit(1).collect()
+				  		  val updated_string = updated_array(0).toString().stripPrefix("[").stripSuffix("]").trim
+
+				  		  //if record is an update, upload latest timestamp in small table
+				  		  if (updated_string == "1") {
+				  		  df.select(primarykey_string, "@timestamp").write.mode("append").parquet(destinationUrl+table_name_string+"/updated_records")
+				  		  val updated_timestamp = sqlContext.read.parquet(destinationUrl+table_name_string+"/updated_records").groupBy(primarykey_string).agg(max($"@timestamp") as "@timestamp")
+				  		  updated_timestamp.write.mode("overwrite").parquet(destinationUrl+table_name_string+"/updated_records")
+				  		  }
 			  				} catch {
 							case NonFatal(t) => println("Waiting for more data")
 							}
 					}
+
+
+					
 			  	if(outputformat == "avro") {
 				  	try {
 			  			val json_rdd =  sqlContext.jsonRDD(rdd.map(_._2))
 			  	  		val df = json_rdd.toDF()
-			  	  		df.write.mode("append").avro(destinationUrl+topics)
+			  	  		df.write.mode("append").avro(destinationUrl+topic)
 		  	  			} catch {
 						case NonFatal(t) => println("Waiting for more data")
 						}
 					}
 				if(outputformat == "json") {
-  				  	rdd.map(_._2).saveAsTextFile(destinationUrl+topics+"/clickstream-"+timestamp+".json")
+  				  	rdd.map(_._2).saveAsTextFile(destinationUrl+topic+"/clickstream-"+timestamp+".json")
 					}
 		}
     })
